4aa71bb1fc2685add5b12a65573920c1e6bf53ed
KatuQA <anhandeo@yandex.ru>
2023-03-14 00:31:09 +0700

    add File fix selectors

--- src/test/java/ru/academit/Homework/DemoqaFormTests.java
+++ src/test/java/ru/academit/Homework/DemoqaFormTests.java
@@ -17,8 +17,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
-import java.util.List;
+
 
 public class DemoqaFormTests {
     private WebDriver driver;
@@ -28,6 +29,8 @@ public class DemoqaFormTests {
     String email = "test@test.com";
     String phoneNumber = "1234567890";
     String address = "Test address";
+
+    File file = new File("src/test/java/ru/academit/Homework/TestIMG.png");
 
 
     @BeforeEach
@@ -65,7 +68,7 @@ public class DemoqaFormTests {
         driver.findElement(By.xpath("//*[@id='userEmail']")).sendKeys(email);
 
         //Gender
-        WebElement radioGender = driver.findElement(By.xpath("//*[@id='genterWrapper']/div[2]/div[2]/label"));
+        WebElement radioGender = driver.findElement(By.xpath("(//*[@id='genterWrapper']//label)[2]"));
         radioGender.click();
         radioGender.isSelected();
 
@@ -74,13 +77,13 @@ public class DemoqaFormTests {
 
         //Birthday
         driver.findElement(By.xpath("//*[@id='dateOfBirthInput']")).click();
-        WebElement dataPickerMonth = driver.findElement(By.xpath("//*[@id='dateOfBirth']/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[1]/select"));
+        WebElement dataPickerMonth = driver.findElement(By.xpath("//*[contains(@class,'react-datepicker__month-select')]"));
         Select selectMonth = new Select(dataPickerMonth);
         selectMonth.selectByIndex(1);
-        WebElement dataPickerYear = driver.findElement(By.xpath("//*[@id='dateOfBirth']/div[2]/div[2]/div/div/div[2]/div[1]/div[2]/div[2]/select"));
+        WebElement dataPickerYear = driver.findElement(By.xpath("//*[contains(@class,'react-datepicker__year-select')]"));
         Select selectYear = new Select(dataPickerYear);
         selectYear.selectByValue("2000");
-        driver.findElement(By.xpath("//*[@id='dateOfBirth']/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div[4]")).click();
+        driver.findElement(By.xpath("//*[contains(@class,'react-datepicker__day--015')]")).click();
 
         //Subjects
         WebElement subject = driver.findElement(By.xpath("//*[@id='subjectsInput']"));
@@ -92,12 +95,10 @@ public class DemoqaFormTests {
 
         //Picture
         WebElement uploadFile = driver.findElement(By.xpath("//*[@id='uploadPicture']"));
-        uploadFile.sendKeys("C:\\Users\\Katu\\IdeaProjects\\selenium-tests\\src\\test\\java\\ru\\academit\\Homework\\TestIMG.png");
-
+        uploadFile.sendKeys(file.getAbsolutePath());
 
         //Address
         driver.findElement(By.xpath("//*[@id='currentAddress']")).sendKeys(address);
-
 
         //State and City
         WebElement selectState = driver.findElement(By.xpath("//*[@id='react-select-3-input']"));
@@ -107,75 +108,78 @@ public class DemoqaFormTests {
         WebElement selectCity = driver.findElement(By.xpath("//*[@id='react-select-4-input']"));
         selectCity.sendKeys("Delhi");
         selectCity.sendKeys(Keys.RETURN);
-    }
 
-    @AfterEach
-    public void stopDriver() throws InterruptedException {
         driver.findElement(By.xpath("//*[@id='submit']")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='example-modal-sizes-title-lg']")));
 
+        // ---- ПРОВЕРКИ ----
+
         //проверка Student Name
-        WebElement student = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr/td[2]"));
+        WebElement student = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[1]/td[2]"));
         student.getText();
         System.out.println(student.getText());
         Assertions.assertEquals(firstName + " " + lastName, student.getText());
 
         //проверка Student Email
-        WebElement studentEmail = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[2]/td[2]"));
+        WebElement studentEmail = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[2]/td[2]"));
         studentEmail.getText();
         System.out.println(studentEmail.getText());
         Assertions.assertEquals(email, studentEmail.getText());
 
         //проверка Gender
-        WebElement studentGender = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[3]/td[2]"));
+        WebElement studentGender = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[3]/td[2]"));
         studentGender.getText();
         System.out.println(studentGender.getText());
         Assertions.assertEquals("Female", studentGender.getText());
 
         //проверка Mobile
-        WebElement studentPhone = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[4]/td[2]"));
+        WebElement studentPhone = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[4]/td[2]"));
         studentPhone.getText();
         System.out.println(studentPhone.getText());
         Assertions.assertEquals(phoneNumber, studentPhone.getText());
 
         //проверка Date of Birth
-        WebElement studentBirthday = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[5]/td[2]"));
+        WebElement studentBirthday = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[5]/td[2]"));
         studentBirthday.getText();
         System.out.println(studentBirthday.getText());
-        Assertions.assertEquals("02 February,2000", studentBirthday.getText());
+        Assertions.assertEquals("15 February,2000", studentBirthday.getText());
 
         //проверка Subjects
-        WebElement studentSubject = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[6]/td[2]"));
+        WebElement studentSubject = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[6]/td[2]"));
         studentSubject.getText();
         System.out.println(studentSubject.getText());
         Assertions.assertEquals("Arts", studentSubject.getText());
 
         //проверка Hobbies
-        WebElement studentHobbies = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[7]/td[2]"));
+        WebElement studentHobbies = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[7]/td[2]"));
         studentHobbies.getText();
         System.out.println(studentHobbies.getText());
         Assertions.assertEquals("Sports", studentHobbies.getText());
 
         //проверка Picture
-        WebElement studentPicture = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[8]/td[2]"));
+        WebElement studentPicture = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[8]/td[2]"));
         studentPicture.getText();
         System.out.println(studentPicture.getText());
         Assertions.assertEquals("TestIMG.png", studentPicture.getText());
 
         //проверка Address
-        WebElement studentAddress = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[9]/td[2]"));
+        WebElement studentAddress = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[9]/td[2]"));
         studentAddress.getText();
         System.out.println(studentAddress.getText());
         Assertions.assertEquals(address, studentAddress.getText());
 
         //проверка State and City
-        WebElement studentState = driver.findElement(By.xpath("//*[@class='table table-dark table-striped table-bordered table-hover']//tbody/tr[10]/td[2]"));
+        WebElement studentState = driver.findElement(By.xpath("//*[contains(@class,'table')]//tbody/tr[10]/td[2]"));
         studentState.getText();
         System.out.println(studentState.getText());
         Assertions.assertEquals("NCR Delhi", studentState.getText());
 
         Thread.sleep(3000);
+    }
+
+    @AfterEach
+    public void stopDriver() {
 
         driver.quit();
     }
